eab67274f38e6b8907f0bf92d937efcfa25b3596
jmosquera <jmosquera@fagorelectronica.com.co>
2024-05-18 23:50:24 -0500

    cambio de url

--- client/src/helper/Global.js
+++ client/src/helper/Global.js
@@ -1,3 +1,3 @@
 export const Global = {
-    url: "https://controlavanceproyectos-jhon-sebastians-projects.vercel.app/api"
+    url: "https://controlavanceproyectos-6foe3bh9b-jhon-sebastians-projects.vercel.app"
 }
